69750ccdcd31122c0d4e660fe45f160319b0c5b0
burdennn <burdennn@163.com>
2020-08-25 22:47:42 +0800

    392.Is Subsequence

---
+++ src/main/java/com/burdennn/No821.java
@@ -0,0 +1,26 @@
+package com.burdennn;
+
+/**
+ * 821. Shortest Distance to a Character {@url https://leetcode.com/problems/shortest-distance-to-a-character/}
+ *
+ * Given a string S and a character C, return an array of integers representing the shortest distance from the character C in the string.
+ *
+ * Example 1:
+ *
+ * Input: S = "loveleetcode", C = 'e'
+ * Output: [3, 2, 1, 0, 1, 0, 0, 1, 2, 2, 1, 0]
+ *
+ */
+
+
+public class No821 {
+    public static void main(String[] args) {
+
+    }
+}
+
+//class Solution {
+//    public int[] shortestToChar(String S, char C) {
+//
+//    }
+//}
---
+++ src/main/java/com/burdennn/NoXX.java
@@ -0,0 +1,15 @@
+package com.burdennn;
+
+/**
+ * 821. Shortest Distance to a Character {@url https://leetcode.com/problems/shortest-distance-to-a-character/}
+ *
+ *
+ *
+ */
+
+
+public class NoXX {
+    public static void main(String[] args) {
+
+    }
+}
